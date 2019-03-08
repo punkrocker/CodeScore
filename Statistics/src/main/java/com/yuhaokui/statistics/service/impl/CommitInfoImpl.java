574d5eb64b1b58271875f00864418963caa43b03
yuhaokui <punkrock_2000@163.com>
2019-03-08 17:55:53 +0800

    获取当月项目提交情况

--- Statistics/src/main/java/com/yuhaokui/statistics/service/impl/CommitInfoImpl.java
+++ Statistics/src/main/java/com/yuhaokui/statistics/service/impl/CommitInfoImpl.java
@@ -6,6 +6,9 @@ import com.yuhaokui.statistics.service.ICommitInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -15,6 +18,20 @@ public class CommitInfoImpl implements ICommitInfo {
 
     @Override
     public List<ProjectCommit> getProjectCommits() {
-        return commitMapper.getProjectCommitCounts("2019-02-01", "2019-02-28");
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH) + 1;
+        //设置年份
+        cal.set(Calendar.YEAR, year);
+        //设置月份
+        cal.set(Calendar.MONTH, month-1);
+        int firstDay = cal.getMinimum(Calendar.DATE);
+        cal.set(Calendar.DAY_OF_MONTH,firstDay);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String beginDate = sdf.format(cal.getTime());
+        int lastDay = cal.getActualMaximum(Calendar.DATE);
+        cal.set(Calendar.DAY_OF_MONTH, lastDay);
+        String endDate = sdf.format(cal.getTime());
+        return commitMapper.getProjectCommitCounts(beginDate, endDate);
     }
 }
