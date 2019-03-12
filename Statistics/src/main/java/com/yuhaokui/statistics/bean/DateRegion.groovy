9015809704d0678b964d55779c30de8dcb79b8cc
yuhaokui <punkrock_2000@163.com>
2019-03-12 15:50:00 +0800

    添加时间区间类，默认构造函数获取当前月起止日期

---
+++ Statistics/src/main/java/com/yuhaokui/statistics/bean/DateRegion.groovy
@@ -0,0 +1,25 @@
+package com.yuhaokui.statistics.bean
+
+import java.text.SimpleDateFormat
+
+class DateRegion {
+    String beginDate
+    String endDate
+
+    DateRegion() {
+        Calendar cal = Calendar.getInstance()
+        int year = cal.get(Calendar.YEAR)
+        int month = cal.get(Calendar.MONTH) + 1
+        //设置年份
+        cal.set(Calendar.YEAR, year)
+        //设置月份
+        cal.set(Calendar.MONTH, month-1)
+        int firstDay = cal.getMinimum(Calendar.DATE)
+        cal.set(Calendar.DAY_OF_MONTH,firstDay)
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd")
+        this.beginDate = sdf.format(cal.getTime())
+        int lastDay = cal.getActualMaximum(Calendar.DATE)
+        cal.set(Calendar.DAY_OF_MONTH, lastDay)
+        this.endDate = sdf.format(cal.getTime())
+    }
+}
