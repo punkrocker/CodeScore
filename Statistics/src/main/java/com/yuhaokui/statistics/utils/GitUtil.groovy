349f0dc613ff61557fc549aa49b8429dbba3f562
yuhaokui <punkrock_2000@163.com>
2019-03-31 17:59:35 +0800

    workspace从配置文件中读取

--- Statistics/src/main/java/com/yuhaokui/statistics/utils/GitUtil.groovy
+++ Statistics/src/main/java/com/yuhaokui/statistics/utils/GitUtil.groovy
@@ -1,10 +1,17 @@
 package com.yuhaokui.statistics.utils
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 
+@Component
 class GitUtil {
 
-    String workSpace
+    static String workSpace
+
+    @Value('${git.workspace}')
+    String setWorkSpace(String workSpace) {
+        this.workSpace = workSpace
+    }
 
     void clone(String gitAddr) {
         ("git clone " + gitAddr).execute()
