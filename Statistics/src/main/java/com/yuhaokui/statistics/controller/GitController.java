7686df6f0d4d2c5bd7bcef2087601bdc1f232128
yuhaokui <punkrock_2000@163.com>
2019-06-06 13:40:08 +0800

    修正clone出错路径也拷贝配置文件问题

--- Statistics/src/main/java/com/yuhaokui/statistics/controller/GitController.java
+++ Statistics/src/main/java/com/yuhaokui/statistics/controller/GitController.java
@@ -1,16 +1,33 @@
 package com.yuhaokui.statistics.controller;
 
+import com.yuhaokui.statistics.bean.ProjectCommit;
+import com.yuhaokui.statistics.service.impl.CommitInfoImpl;
+import com.yuhaokui.statistics.utils.AppConst;
 import com.yuhaokui.statistics.utils.GitUtil;
+import com.yuhaokui.statistics.utils.SonarUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/git")
 public class GitController {
+    @Autowired
+    CommitInfoImpl commitInfos;
 
     @RequestMapping("/")
     public String index() {
         GitUtil gitUtil = new GitUtil();
-        return gitUtil.clone("yuhaokui/testproject2");
+        SonarUtil sonarUtil = new SonarUtil();
+        List<ProjectCommit> projectCommits = commitInfos.getFinalCommits();
+        projectCommits.forEach((commitInfo) -> {
+            String workPath = gitUtil.clone(commitInfo.getGitPath());
+            if (!workPath.startsWith(AppConst.ERROR_INFO))
+                System.out.println(sonarUtil.checkConfigFile(workPath));
+        });
+        return "success";
     }
 }
