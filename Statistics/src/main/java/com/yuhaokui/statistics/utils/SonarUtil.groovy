bbd038217421fdc85bafc63b04f433c311d42cc6
yuhaokui <punkrock_2000@163.com>
2019-06-06 16:07:19 +0800

    sonar模板改为直接使用多行字符串拼接

--- Statistics/src/main/java/com/yuhaokui/statistics/utils/SonarUtil.groovy
+++ Statistics/src/main/java/com/yuhaokui/statistics/utils/SonarUtil.groovy
@@ -12,9 +12,20 @@ class SonarUtil {
         String filePath = dir + File.separatorChar + 'sonar-project.properties'
         File configFile = new File(filePath)
         if (!configFile.exists()) {
-            File file = new File('src/main/resources/sonar-template')
-            Files.copy(Paths.get(file.path), Paths.get(filePath))
+            changeFileContent(configFile)
         }
         filePath
+    }
+
+    def changeFileContent(File file) {
+        String[] paths = file.path.split(File.separatorChar as String)
+        //至少有group和project两级
+        assert paths.length >= 2
+        StringBuilder sb = new StringBuilder()
+        sb.append('sonar.projectKey=' + paths[-2] + '\n')
+        sb.append('sonar.projectName=' + paths[-3] + paths[-2] + '\n')
+        sb.append('sonar.projectVersion=1.0' + '\n')
+        sb.append('sonar.sources=src' + '\n')
+        file.write(sb.toString())
     }
 }
