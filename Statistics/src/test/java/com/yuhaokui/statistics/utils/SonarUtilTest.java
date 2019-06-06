14c679117cc6bfce95681d159f4e01eb412a576e
yuhaokui <punkrock_2000@163.com>
2019-06-06 14:39:13 +0800

    添加BeforeEach注解删除已拷贝的properties文件

--- Statistics/src/test/java/com/yuhaokui/statistics/utils/SonarUtilTest.java
+++ Statistics/src/test/java/com/yuhaokui/statistics/utils/SonarUtilTest.java
@@ -1,10 +1,21 @@
 package com.yuhaokui.statistics.utils;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SonarUtilTest {
+    @BeforeEach
+    void cleanFile() {
+        String templatePath = System.getProperty("user.dir") + File.separatorChar + "sonar-project.properties";
+        File configFile = new File(templatePath);
+        if (configFile.exists())
+            configFile.delete();
+    }
 
     @Test
     void checkConfigFile() {
